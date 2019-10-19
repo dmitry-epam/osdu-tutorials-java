451a05bf5e1280eacd06187d195b9fb0ae34483d
Biswajeet Basumatary <biswajeet.basumatary@gmail.com>
2019-10-18 19:45:17 -0500

    Authorization class.

---
+++ src/test/java/quickstart/auth/AppTest.java
@@ -0,0 +1,19 @@
+package quickstart.auth;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void testApp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBearerToken() {
+		fail("Not yet implemented");
+	}
+
+}
