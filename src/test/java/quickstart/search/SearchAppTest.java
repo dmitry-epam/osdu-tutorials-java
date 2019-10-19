2f6333707bb253f7414e22fe6210fe9e58e52d58
Biswajeet Basumatary <biswajeet.basumatary@gmail.com>
2019-10-19 09:03:14 -0500

    Commit 2

---
+++ src/test/java/quickstart/search/SearchAppTest.java
@@ -0,0 +1,14 @@
+package quickstart.search;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SearchAppTest {
+
+	@Test
+	void testFindAWell() {
+		fail("Not yet implemented");
+	}
+
+}
